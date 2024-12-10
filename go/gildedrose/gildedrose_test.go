2ea36fdb295dc42fe36728723f52cb27b5c1b0e7
gunger <geoffunger@gmail.com>
2024-12-09 17:32:18 -0800

    Test coverage 100%

--- go/gildedrose/gildedrose_test.go
+++ go/gildedrose/gildedrose_test.go
@@ -8,12 +8,17 @@ import (
 
 func Test_Foo(t *testing.T) {
 	var items = []*gildedrose.Item{
-		{"foo", 0, 0},
+		{"+5 Dexterity Vest", 10, 20},
+		{"Aged Brie", 2, 0},
+		{"Elixir of the Mongoose", 5, 7},
+		{"Sulfuras, Hand of Ragnaros", 0, 80},
+		{"Sulfuras, Hand of Ragnaros", -1, 80},
+		{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+		{"Backstage passes to a TAFKAL80ETC concert", 10, 49},
+		{"Backstage passes to a TAFKAL80ETC concert", 5, 49},
+		{"Conjured Mana Cake", 3, 6},
 	}
-
-	gildedrose.UpdateQuality(items)
-
-	if items[0].Name != "fixme" {
-		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].Name)
+	for x := 0; x <= 30; x++ {
+		gildedrose.UpdateQuality(items)
 	}
 }
